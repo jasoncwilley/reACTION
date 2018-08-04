37d41ac3b796821a8bb5f7ae48128bc608912c16
minty <jason.c.willey@gmail.com>
2018-08-04 14:10:21 -0400

    added note component

---
+++ src/Note/Note.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './Note.css';
+import PropTypes from 'prop-types';
+
+class Note extends Component{
+
+    constructor(props){
+        super(props);
+        this.message = "hello from the Note component"
+    }
+
+    render(props) {
+        return(
+            <div>
+               <h1>{this.message}</h1>
+            </div>   
+        )
+    }
+}
+
+Note.propTypes = {
+
+}
+
+export default Note;
